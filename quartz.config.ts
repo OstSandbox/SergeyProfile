65f0c10ea716e1edf9033be86c46e27dd0663312
OstSandbox <zartanin@bk.ru>
2025-04-04 21:51:58 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -86,6 +86,10 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
+      Plugin.Component.Script({
+  src: "/js/password-protection.js",
+  loadTime: "beforeDOMReady",
+}),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
